1d1274b43e93ecccd00b6d8b1954211d51069d3e
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-08 12:36:04 +0500

    a new component created and called

---
+++ src/components/newcomp.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+@Component
+export default class newcomp extends Vue {
+  msg = "Hello";
+}
+</script>
